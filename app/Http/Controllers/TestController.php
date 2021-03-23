a96eb1d8251ea3b8b23fba159b01f1a71623cc87
Irrigard <iektots@gmail.com>
2021-03-23 23:06:15 +0300

    Learning Blade If statements

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,6 @@ class TestController extends Controller
 {
     public function show()
     {
-        return view('test.show', ['arr' => ['country' => 'United Kingdom', 'city' => 'London', 'age'=>30, 'salary'=>30000, 'str'=>'Trusted string']]);
+        return view('test.show', ['arr' => ['country' => 'United Kingdom', 'city' => 'London', 'age'=>30, 'salary'=>30000, 'str'=>'Trusted string'], 'weekDay'=>3, 'month'=>5, 'numArr'=>[1, 2]]);
     }
 }
--- resources/views/test/show.blade.php
+++ resources/views/test/show.blade.php
@@ -9,9 +9,42 @@
     Дата: {{ $arr['day'] ?? date('d', time()) }}.{{ $arr['month'] ?? date('m', time()) }}.{{ $arr['year'] ?? date('Y', time()) }}<br>
     Неэкранированные данные: {!! $arr['str'] !!}<br>
     {{-- Commented  string--}}
-@php
-    echo 'Вставка PHP';
-@endphp
+    @php
+        echo 'Вставка PHP';
+    @endphp
+
+    @if(isset($weekDay))
+        @if($weekDay < 5)
+            <p>Рабочий день</p>
+        @else
+            <p>Выходной!</p>
+        @endif
+    @endif
+
+    @if(isset($month))
+        @if($month > 1 && $month < 5)
+            <p>Весна</p>
+        @elseif($month > 4 && $month < 8)
+            <p>Лето</p>
+        @elseif($month > 7 && $month < 11)
+            <p>Осень</p>
+        @elseif($month >= 0 && $month < 2 || $month === 11)
+            <p>Зима</p>
+        @else
+            <p>Wrong month value</p>
+        @endif
+    @endif
+
+    @unless($arr['age'] > 18)
+        <p>Вам меньше 18 лет</p>
+    @endunless
+
+    @if(count($numArr) > 0)
+        <p>{{ array_sum($numArr) }}</p>
+    @else
+        <p>Массив пуст</p>
+    @endif
+
 
 </body>
 </html>
