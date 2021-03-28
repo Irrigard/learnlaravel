b86d2315403fa0c819be75c02b31cf7ed73fe5ea
Irrigard <iektots@gmail.com>
2021-03-28 19:56:12 +0300

    Learning Blade loops

--- resources/views/test/show.blade.php
+++ resources/views/test/show.blade.php
@@ -45,6 +45,79 @@
         <p>Массив пуст</p>
     @endif
 
+<ul>
+    @foreach($numArr as $elem)
+        <li>{{ sqrt($elem) }}</li>
+    @endforeach
+</ul>
+<ul>
+    @foreach($strArr as $key=>$elem)
+        <li>{{ $key + 1 }} {{ $elem }}</li>
+    @endforeach
+</ul>
+    <ul>
+        @foreach($numArr as $key=>$elem)
+            @if($elem % 2 == 0)
+            <li>{{ $elem }}</li>
+            @endif
+        @endforeach
+    </ul>
+@if(is_array($data))
+    <ul>
+        @foreach($data as $elem)
+            <li>{{ $elem }}</li>
+        @endforeach
+    </ul>
+@else
+    <p>{{ $data }}</p>
+@endif
 
+<table>
+    <tr>
+    @for($i=1; $i<=25; $i++)
+        <td>{{ $users[$i-1] }}</td>
+        @if($i%5 == 0)
+            </tr><tr>
+        @endif
+    @endfor
+    </tr>
+</table>
+<table>
+    @foreach($employees as $employee)
+        <tr>
+            <td>{{ $employee['name'] }} {{ $employee['surname'] }} {{ $employee['salary'] }}</td>
+        </tr>
+    @endforeach
+</table>
+<ul>
+    @foreach($strArr as $str)
+        <ul @if($loop->first)
+                class="first"
+            @elseif($loop->last)
+                class="last"
+            @endif
+            >{{ $loop->iteration }} {{ $str }}</ul>
+    @endforeach
+</ul>
+@foreach($numArr as $num)
+    @if($loop->remaining <= 2)
+        <i> {{$num}} </i>
+    @else
+        <b>{{ $num }}</b>
+    @endif
+@endforeach
+
+<ul>
+    @foreach ($employees as $employee)
+        @continue($employee['surname'] == 'surname1')
+        @break($employee['salary'] == 3000)
+        <li>{{ $employee['name'] }}</li>
+    @endforeach
+</ul>
+@forelse($emptyArr as $elem)
+    {{$elem}}
+@empty
+    <p>В массиве нет элементов</p>
+@endforelse
 </body>
 </html>
